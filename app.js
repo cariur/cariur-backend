53778815af09f397a8823a507ff4eb50bdead257
Kasim Saifi <kasimytceation@gmail.com>
2024-09-21 23:13:55 +0530

    updated

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     },
   })
 );
